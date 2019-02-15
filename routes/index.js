90a5a66c6746461cb62431158a86994a208dd046
broderick higby <broderick.higby@gmail.com>
2019-02-14 18:50:26 -0800

    merging poseest

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,20 @@
-var data = require('../data.json');
+
+/*
+ * GET home page.
+ */
+
+// exports.view = function(req, res){
+//   res.render('index', 
+//   {
+//     'image': 'profileIcon.png',
+//     'id': 'profileicon'
+//   },{},
+//   {
+//     'image': 'downarrow.jpeg',
+//     'id': 'downarrow'
+//   });
+// };
+
 exports.view = function(req,res){
-  res.render('index', data);
+  res.render('index');
 };

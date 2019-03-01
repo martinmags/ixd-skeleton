22a41ac5497378c9075d59618638bc160cb4cadc
broderick higby <broderick.higby@gmail.com>
2019-02-28 21:52:24 -0800

    updated handicap

--- public/js/camera/camera.js
+++ public/js/camera/camera.js
@@ -2,15 +2,19 @@ let video;
 let canvas;
 let ctx;
 let network;
+var address = false;
+var top = false;
+var impact = false;
+var finishSwing = false;
 
 function startVideo() {
     network = posenet.load();
     video = document.getElementById("camera");
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    canvas.setAttribute("width", 500);
-    canvas.setAttribute("height", 500);
-        
+    canvas.setAttribute("width", 600);
+    canvas.setAttribute("height", 600);
+
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
             video.srcObject = stream;
@@ -19,7 +23,7 @@ function startVideo() {
     }
 
     renderFrame();
-    window.setInterval(renderFrame, 10);
+    window.setInterval(renderFrame, 1);
     
 
 }
@@ -42,7 +46,6 @@ function renderFrame() {
 const connectedPartNames = [ ['leftHip', 'leftShoulder'], ['leftElbow', 'leftShoulder'], ['leftElbow', 'leftWrist'], ['leftHip', 'leftKnee'], ['leftKnee', 'leftAnkle'], ['rightHip', 'rightShoulder'], ['rightElbow', 'rightShoulder'], ['rightElbow', 'rightWrist'], ['rightHip', 'rightKnee'], ['rightKnee', 'rightAnkle'], ['leftShoulder', 'rightShoulder'], ['leftHip', 'rightHip'] ];
 const partNames = [ 'nose', 'leftEye', 'rightEye', 'leftEar', 'rightEar', 'leftShoulder', 'rightShoulder', 'leftElbow', 'rightElbow', 'leftWrist', 'rightWrist', 'leftHip', 'rightHip', 'leftKnee', 'rightKnee', 'leftAnkle', 'rightAnkle' ];
 const partIds = partNames.reduce((result, jointName, i) => { result[jointName] = i; return result; }, {});
-
 const connectedPartIndices = connectedPartNames.map(([jointNameA, jointNameB]) => ([partIds[jointNameA], partIds[jointNameB]]));
 
 function eitherPointDoesntMeetConfidence( a, b, minConfidence) {
@@ -87,54 +90,66 @@ function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
 function drawSkeleton(keypoints, minConfidence, scale = 1) {
   const adjacentKeyPoints =
       getAdjacentKeyPoints(keypoints.keypoints, minConfidence);
+  didAddressBall(keypoints);
   adjacentKeyPoints.forEach((keypoints) => {
     drawSegment(
         toTuple(keypoints[0].position), toTuple(keypoints[1].position), "#00FF00",
         scale, ctx);
   });
-  didAddressBall(keypoints);
+
 }
 
 function didAddressBall(keypoints) {
   var wristDistance = (((keypoints.keypoints[9]["position"].x - keypoints.keypoints[10]["position"].x + keypoints.keypoints[9]["position"].y - keypoints.keypoints[10]["position"].y)/2));
   var wrists = ((keypoints.keypoints[9]["position"].y + keypoints.keypoints[10]["position"].y)/2);
-  var didAddressBall = false;
-  var topOfBackswing = false;
-  var impactPosition = false;
-  var finishSwing = false;
+
   // Check that the keypoints confidence levels are high enough
   if (keypoints.keypoints[9]["score"] > .8 && 
       keypoints.keypoints[10]["score"] > .8 &&
       wristDistance < 30)  {
       console.log("Golfer has addressed ball");
-      didAddressBall = true;
-    }
-    if (wrists < 200 && didAddressBall == true) {
+      address = true;
+    if (wrists < 200 && address == true) {
       console.log("Golfer reached top of backswing");
-      topOfBackswing = true;
-    // }
-    // if (wrists > 300 && topOfBackswing == true) {
-    //   console.log("Golfer in impact position");
-    //   impactPosition = true;
-    // } 
-    // if (wrists < 200 && impactPosition == true) {
-    //   console.log("Golfer has finished golf swing");
-    // }
-      var firstAngle = Math.atan2((keypoints.keypoints[5]["position"].y - keypoints.keypoints[11]["position"].y),(keypoints.keypoints[5]["position"].x - keypoints.keypoints[11]["position"].x));
-      var secondAngle = Math.atan2((keypoints.keypoints[6]["position"].y - keypoints.keypoints[11]["position"].y),(keypoints.keypoints[6]["position"].x - keypoints.keypoints[11]["position"].x));
-      var angle = (firstAngle - secondAngle);
-      console.log(angle)
-
-      didAddressBall = false;
-      // topOfBackswing = false;
-      // impactPosition = false;
+        address = false;
+      top = true;
     }
-    if (topOfBackswing == true) {
-                responsiveVoice.speak("Your form looks good, just remember your front arm and back arm should be as straight as possible at impact with your front arm perfectly in line with your club shaft Your back shoulder should be dropped a bit below the height of your front shoulder This will allow your hands to remain ahead of your club head and your back foot to lift off the ground.");
+    if (wrists > 300 && top == true) {
+      console.log("Golfer in impact position");
+        top = false;
+      impact = true;
+    } 
+    if (wrists < 200 && impact == true) {
+      console.log("Golfer has finished golf swing");
+      impact = false;
+      finishSwing = true;
     }
-      // keypoints.keypoints[8]["position"].x && keypoints.keypoints[9]["position"].x
-      // console.log(keypoints.keypoints[9]["position"].y);
-    // responsiveVoice.speak("Your form looks good, just remember your front arm and back arm should be as straight as possible at impact, with your front arm perfectly in line with your club shaft. Your back shoulder should be dropped a bit below the height of your front shoulder. This will allow your hands to remain ahead of your club head, and your back foot to lift off the ground.");
-
-
+    if (finishSwing == true) {
+        angleMeasurement(keypoints);
+        voiceDidSpeak();
+    }
+  }
 }
+
+function voiceDidSpeak() {
+      let hips = "Your form looks good turn your hips more in your downswing";
+      let straightArms = "just remember your front arm and back arm should be as straight as possible at impact";
+      let frontArm = "put your front arm perfectly in line with your club shaft at impact.";
+      let shoulders = "Your back shoulder should be dropped a bit below the height of your front shoulder";
+      let hooking = "This will allow your hands to remain ahead of your club head and your back foot to lift off the ground.";
+      var i;
+      var body = [hips, straightArms,frontArm,shoulders,hooking];
+      var text = ""
+      for (i = 0; i < body.length; i++) {
+          text = String.valueOf(body[i]);
+          responsiveVoice.speak(text);
+      }
+}
+function angleMeasurement(keypoints) {
+    var firstAngle = Math.atan2((keypoints.keypoints[5]["position"].y - keypoints.keypoints[11]["position"].y),(keypoints.keypoints[5]["position"].x - keypoints.keypoints[11]["position"].x));
+    var secondAngle = Math.atan2((keypoints.keypoints[6]["position"].y - keypoints.keypoints[11]["position"].y),(keypoints.keypoints[6]["position"].x - keypoints.keypoints[11]["position"].x));
+    var angle = (firstAngle - secondAngle);
+    console.log(angle);
+}
+
+
